bb04b01948cea07446f05233aa9379c4a18e25f9
unknown <thomas91714@gmail.com>
2022-05-15 22:15:18 +0800

    first red bugfix

--- apps/bowling/src/app/game.spec.ts
+++ apps/bowling/src/app/game.spec.ts
@@ -1,9 +1,11 @@
 import { Book } from './game';
 
 describe('Book', () => {
-  const book = new Book();
-  for (let i = 0; i < 20; i++) {
-    book.buy(0);
-  }
-  expect(book.price).toBe(0);
+  test('gutter game', () => {
+    const book = new Book();
+    for (let i = 0; i < 20; i++) {
+      book.buy(0);
+    }
+    expect(book.price).toBe(0);
+  });
 });
