1655fedddefdbe0b3293879b3176ccf1a60dfc81
unknown <thomas91714@gmail.com>
2022-05-15 22:17:01 +0800

    second red

--- apps/bowling/src/app/game.spec.ts
+++ apps/bowling/src/app/game.spec.ts
@@ -1,11 +1,18 @@
 import { Book } from './game';
 
 describe('Book', () => {
-  test('gutter game', () => {
+  test('buy zero book', () => {
     const book = new Book();
     for (let i = 0; i < 20; i++) {
       book.buy(0);
     }
     expect(book.price).toBe(0);
+  });
+  test('buy one book', () => {
+    const book = new Book();
+    for (let i = 0; i < 20; i++) {
+      book.buy(1);
+    }
+    expect(book.price).toBe(100);
   });
 });
