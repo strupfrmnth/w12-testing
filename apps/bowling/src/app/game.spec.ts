d4cfbc19c6479c7dde4f743ac3ec43e9cd7ba9bc
unknown <thomas91714@gmail.com>
2022-05-15 22:23:33 +0800

    first reconstruct

--- apps/bowling/src/app/game.spec.ts
+++ apps/bowling/src/app/game.spec.ts
@@ -1,13 +1,21 @@
 import { Book } from './game';
 
 describe('Book', () => {
+  let book: Book;
+
+  beforeEach(() => {
+    book = new Book();
+  });
+
+  it('should create an instance', () => {
+    expect(book).toBeTruthy();
+  });
+
   test('buy zero book', () => {
-    const book = new Book();
     book.buy(0);
     expect(book.price).toBe(0);
   });
   test('buy one book', () => {
-    const book = new Book();
     book.buy(1);
     expect(book.price).toBe(100);
   });
